032ffa277f59956362d7c44477066db5354e7c58
Morijuu <pmorellviadel@gmail.com>
2026-04-30 10:02:43 +0200

    Small World

--- Assets/Scripts/Forces/SmallWorld.cs
+++ Assets/Scripts/Forces/SmallWorld.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class SmallWorld : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float G = 9.8f; // constante gravitacional (puedes escalarla)
+
+    Rigidbody rb;
+    Rigidbody earthRb;
+    public GameObject earth;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        earthRb = earth.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate() // usar física aquí, no Update
     {
-        
+        Vector3 direction = earth.transform.position - transform.position;
+        float distance = direction.magnitude;
+
+        // Evitar división por cero
+        if (distance == 0f) return;
+
+        // Calcular fuerza gravitatoria
+        float forceMagnitude = (G * rb.mass * earthRb.mass) / (distance * distance);
+
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rb.AddForce(force);
     }
 }
